92dd28b01ca893339b6d3a35e4c67bac52d145dd
Marc Espinosa <marespinosa@ebay.com>
2014-11-16 20:57:20 +0100

    Added position class

---
+++ src/whs/Utility/Position/Position2D.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace whs\Utility\Position;
+
+class Position2D
+{
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function x()
+    {
+        return $this->x;
+    }
+
+    public function y()
+    {
+        return $this->y;
+    }
+
+    public function __toString()
+    {
+        return 'X: '.$this->x.' Y: '.$this->y;
+    }
+}
